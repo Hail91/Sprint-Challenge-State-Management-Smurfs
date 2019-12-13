b77243382a65341a015ab08f7ec2ecc61520cfa0
Aaron <aarongillies88@gmail.com>
2019-12-13 13:55:23 -0500

    Submit for MVP

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,40 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
-class App extends Component {
-  render() {
+import { connect } from "react-redux";
+import { getSmurfs } from '../actions';
+import AddSmurf from '../components/AddSmurf';
+
+
+
+const App = (props) =>  {
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <h1>The Village People</h1>
+        {!props.App && !props.isFetching && <p>See the Village!</p>}
+        <div className="village">
+          {props.smurfs.map(info => {
+            return (
+              <div class="card" style={{width: '18rem'}}>
+                <div class="card-body">
+                  <h5 class="card-title">Name: {info.name}</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">Age: {info.age}</h6>
+                  <p class="card-text">Height: {info.height}</p>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+        <button className="add-btn" onClick={props.getSmurfs}>Unveil Smurfs</button>
+        <AddSmurf />
       </div>
     );
+};
+
+const mapStateToProps = state => {
+  return {
+    smurfs: state.smurfs,
+    isFetching: state.isFetching
   }
 }
 
-export default App;
+export default connect(mapStateToProps, { getSmurfs })(App);
